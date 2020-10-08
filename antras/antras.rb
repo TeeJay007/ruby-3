fae34fc4bd59eb5f450fcf15faae9ec24f3b2a89
boss <17435461+TeeJay007@users.noreply.github.com>
2020-10-08 14:33:15 +0300

    update antras.rb

--- antras/antras.rb
+++ antras/antras.rb
@@ -1,20 +1,48 @@
-puts "Iveskite triju krastiniu ilgius:"
-a = gets.chomp.to_i
-b = gets.chomp.to_i
-c = gets.chomp.to_i
+# frozen_string_literal: true
 
-if a > 0 && b > 0 && c > 0
-	p = (a + b + c) / 2.0
-	plotas = Math.sqrt(p * (p - a) * (p - b) * (p - c))
-	if a != b && b != c && a != b && plotas > 0
-		puts "Trikamis yra ivairiakrastis ir jo plotas: #{plotas}"
-	elsif a == b && b == c && a == b && plotas > 0
-		puts "Trikamis yra lygiakrastis ir jo plotas: #{plotas}"
-	elsif (a == b || b == c || a == c) && plotas > 0
-		puts "Trikamis yra lygiasonis ir jo plotas: #{plotas}"
-	else
-		puts "Trikampio sudaryti neimanoma"
-	end
-else
-	puts "Trikampio sudaryti neimanoma"
+# Class to get the type of a triangle
+class Triangles
+  def initialize(a_str, b_str, c_str)
+    @a = a_str.chomp.to_i
+    @b = b_str.chomp.to_i
+    @c = c_str.chomp.to_i
+  end
+
+  def getarea
+    p = (@a + @b + @c) / 2.0
+    Math.sqrt(p * (p - @a) * (p - @b) * (p - @c))
+  end
+
+  def scalene?
+    @a != @b && @b != @c && @a != @b
+  end
+
+  def isosceles?
+    @a == @b || @b == @c || @a == @c
+  end
+
+  def equilateral?
+    @a == @b && @b == @c && @a == @b
+  end
+
+  def invalid?
+    @a.negative? || @b.negative? || @c.negative?
+  end
+
+  def type
+    return 'Trikampio sudaryti neimanoma' if invalid?
+
+    area = getarea
+    if area.positive?
+      return "Trikamis yra ivairiakrastis ir jo plotas: #{area}" if scalene?
+      return "Trikamis yra lygiakrastis ir jo plotas: #{area}" if equilateral?
+      return "Trikamis yra lygiasonis ir jo plotas: #{area}" if isosceles?
+    end
+
+    'Trikampio sudaryti neimanoma'
+  end
 end
+
+puts 'Iveskite triju krastiniu ilgius:'
+t = Triangles.new(gets, gets, gets)
+puts t.type
